23fb62a6d7183403fd98086015b0995079d1b4d0
Arkadiusz Plumbaum <arekpl28@wp.pl>
2019-04-04 15:51:17 +0200

    Adding squares

---
+++ 02 Adding squares/index.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector('button');
+
+btn.addEventListener("click", function () {
+  const divItem = document.createElement("div");
+  document.body.appendChild(divItem);
+})
